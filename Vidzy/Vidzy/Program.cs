882bb34fee1503f4a32398e4f6a267109b488bde
Omkar Ubale <oubale@gmail.com>
2020-04-05 22:40:02 +0530

    Excercise 1 - Database First: completed

---
+++ Vidzy/Vidzy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyEntities();
+
+            context.spAddVideo("Code with Mosh 1", DateTime.UtcNow.AddDays(-1), 2, 3);
+            context.spAddVideo("Code with Mosh 2", DateTime.UtcNow.AddDays(-2), 2, 3);
+            context.spAddVideo("Code with Mosh 3", DateTime.UtcNow.AddDays(-3), 2, 3);
+
+        }
+    }
+}
